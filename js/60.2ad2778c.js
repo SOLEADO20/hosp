58e31f863719c394978ac001e955cec1d6bcb6a3
SOLEADO20 <96033686+SOLEADO20@users.noreply.github.com>
2024-06-24 02:05:21 +0800

    Re-build.

--- js/60.2ad2778c.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkhosp"]=self["webpackChunkhosp"]||[]).push([[60],{2060:function(n,e,s){s.r(e),s.d(e,{default:function(){return o}});var r=s(3396);const t={class:"index"};function a(n,e,s,a,c,u){return(0,r.wg)(),(0,r.iD)("div",t,"欢迎使用，科王飙医院 门诊服务！")}var c={name:"Index"},u=s(89);const d=(0,u.Z)(c,[["render",a],["__scopeId","data-v-23787941"]]);var o=d}}]);
-//# sourceMappingURL=60.2ad2778c.js.map
